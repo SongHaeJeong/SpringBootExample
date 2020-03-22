12a8aa86618b23340a989834edab68080fbc219a
SongHaeJeong <cjsonghae@naver.com>
2020-03-22 15:11:49 +0900

    [ADD][MOD] file

--- BackEnd/src/main/java/com/rest/model/User.java
+++ BackEnd/src/main/java/com/rest/model/User.java
@@ -34,29 +34,8 @@ public class User {
 	
 	@NotBlank
 	@Column(name ="EMAIL")
-	private String email;
-	
-	@NotBlank
-	@Column(name ="PASSWORD")
-	private String password;
-	
-	@NotBlank
-	@Column(name ="NAME")
-	private String name;
-	
-	@NotBlank
-	@Column(name ="NICKNAME")
-	private String	nickName;
-	
-	//자동으로 회원정보 생성한 시간 입력
-	@CreationTimestamp
-	@Column(name ="CREATEDATE")
-	private Timestamp createDate;
-	
-	//자동으로 회원정보 수정한 시간 입력 -> 나중에 몇일이상 수정을 안했을 시 회원 수정 기능 추가 할 예정
-	@UpdateTimestamp
-	@Column(name ="UPDATEDATE")
-	private Timestamp updateDate;
+	private String email;	
+
 	
 	@OneToMany(fetch = FetchType.LAZY ) // 지연 로딩
 	@JoinColumn(name="PORTFOLIO_ID")
